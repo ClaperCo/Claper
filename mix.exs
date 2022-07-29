5ba72d4d3b40a8b6c81aeec5bd4678fad1683ccb
Alex <contact@alexlion.co>
2022-07-29 19:14:15 +0200

    Upgrade dart sass to fix docker image on ARM

--- mix.exs
+++ mix.exs
@@ -99,7 +99,7 @@ defmodule Claper.MixProject do
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.6"},
       {:esbuild, "~> 0.2", runtime: Mix.env() == :dev},
-      {:dart_sass, "~> 0.4", runtime: Mix.env() == :dev},
+      {:dart_sass, "~> 0.5", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:finch, "~> 0.8"},
       {:telemetry_metrics, "~> 0.6"},
