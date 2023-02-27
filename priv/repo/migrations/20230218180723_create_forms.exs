25639c421f7b3c18dc0cef1f7f29f390e8cca433
Alex <dev@alexandrelion.com>
2023-02-27 09:58:21 +0100

    Fix form migration

--- priv/repo/migrations/20230218180723_create_forms.exs
+++ priv/repo/migrations/20230218180723_create_forms.exs
@@ -6,7 +6,7 @@ defmodule Claper.Repo.Migrations.CreateForms do
       add :title, :string, null: false
       add :position, :integer, default: 0
       add :enabled, :boolean, default: true
-      add :presentation_file_id, references(:presentation_files, on_delete: :nothing)
+      add :presentation_file_id, references(:presentation_files, on_delete: :nilify_all)
       add :fields, :map, default: "[]"
 
       timestamps()
