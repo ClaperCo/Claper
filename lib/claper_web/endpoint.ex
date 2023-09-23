648f3a57dfecbe773eca80340601d1269d78ee54
Alex <dev@alexandrelion.com>
2023-09-23 12:03:45 +0200

    Remove unused uploads directoy

--- lib/claper_web/endpoint.ex
+++ lib/claper_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule ClaperWeb.Endpoint do
     at: "/",
     from: :claper,
     gzip: false,
-    only: ~w(assets fonts .well-known uploads images favicon.ico robots.txt loaderio-eb3b956a176cdd4f54eb8570ce8bbb06.txt)
+    only: ~w(assets fonts .well-known images favicon.ico robots.txt loaderio-eb3b956a176cdd4f54eb8570ce8bbb06.txt)
 
   plug Plug.Static,
     at: "/uploads",
