9f0050a2de066aee44a13035ac55d747444de171
Alex Lion <dev@alexandrelion.com>
2024-12-22 11:56:38 -0500

    Add missing lti column

--- priv/repo/migrations/20241207115352_add_lti_line_item_columns_to_quizzes_and_lti_resources.exs
+++ priv/repo/migrations/20241207115352_add_lti_line_item_columns_to_quizzes_and_lti_resources.exs
@@ -4,6 +4,7 @@ defmodule Claper.Repo.Migrations.AddLtiLineItemColumnsToQuizzesAndEvents do
   def change do
     alter table(:quizzes) do
       add :lti_line_item_url, :string
+      add :lti_resource_id, references(:lti_13_resources, on_delete: :delete_all)
     end
 
     alter table(:lti_13_resources) do
