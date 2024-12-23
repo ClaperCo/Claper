b432d34f509ec669e4600e7b3c687c5a0c5fd1de
Alex Lion <dev@alexandrelion.com>
2024-12-23 10:06:45 -0500

    Remove unused line

--- priv/repo/migrations/20241207115352_add_lti_line_item_columns_to_quizzes_and_lti_resources.exs
+++ priv/repo/migrations/20241207115352_add_lti_line_item_columns_to_quizzes_and_lti_resources.exs
@@ -4,7 +4,6 @@ defmodule Claper.Repo.Migrations.AddLtiLineItemColumnsToQuizzesAndEvents do
   def change do
     alter table(:quizzes) do
       add :lti_line_item_url, :string
-      add :lti_resource_id, references(:lti_13_resources, on_delete: :delete_all)
     end
 
     alter table(:lti_13_resources) do
