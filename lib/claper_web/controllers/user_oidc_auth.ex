2f852b498f821be9f0d399a2fae31c07b1720062
Rohan Mishra <315scisyb2020rohanmishra@gmail.com>
2025-03-08 10:03:03 +0530

    Update user_oidc_auth.ex
    /claim #129 
    Fixes: #129

--- lib/claper_web/controllers/user_oidc_auth.ex
+++ lib/claper_web/controllers/user_oidc_auth.ex
@@ -1,6 +1,6 @@
 defmodule ClaperWeb.UserOidcAuth do
   @moduledoc """
-    Plug for OpenID Connect authentication.
+  Plug for OpenID Connect authentication.
   """
   alias ClaperWeb.UserAuth
   use ClaperWeb, :controller
@@ -9,12 +9,22 @@ defmodule ClaperWeb.UserOidcAuth do
 
   @doc false
   def new(conn, _params) do
+    pkce_verifier = generate_pkce_verifier()
+    state = :crypto.strong_rand_bytes(16) |> Base.url_encode64(padding: false)
+    nonce = :crypto.strong_rand_bytes(16) |> Base.url_encode64(padding: false)
+
+    conn =
+      conn
+      |> put_session(:pkce_verifier, pkce_verifier)
+      |> put_session(:oidc_state, state)
+      |> put_session(:oidc_nonce, nonce)
+
     {:ok, redirect_uri} =
       Oidcc.create_redirect_url(
         Claper.OidcProviderConfig,
         client_id(),
         client_secret(),
-        opts()
+        opts(pkce_verifier, state, nonce)
       )
 
     uri = Enum.join(redirect_uri, "")
@@ -22,29 +32,40 @@ defmodule ClaperWeb.UserOidcAuth do
     redirect(conn, external: uri)
   end
 
-  def callback(conn, %{"code" => code} = _params) do
-    with {:ok,
-          %Oidcc.Token{
-            id: %Oidcc.Token.Id{token: id_token, claims: claims},
-            access: %Oidcc.Token.Access{token: access_token},
-            refresh: refresh_token
-          }} <-
-           Oidcc.retrieve_token(
-             code,
-             Claper.OidcProviderConfig,
-             client_id(),
-             client_secret(),
-             opts()
-           ),
-         {:ok, oidc_user} <- validate_user(id_token, access_token, refresh_token, claims) do
+  def callback(conn, %{"code" => code, "state" => returned_state} = _params) do
+    pkce_verifier = get_session(conn, :pkce_verifier)
+    expected_state = get_session(conn, :oidc_state)
+
+    if returned_state != expected_state do
       conn
-      |> UserAuth.log_in_user(oidc_user.user)
+      |> put_status(:unauthorized)
+      |> put_view(ClaperWeb.ErrorView)
+      |> render("csrf_error.html", %{error: "Invalid state parameter"})
     else
-      {:error, reason} ->
+      with {:ok,
+            %Oidcc.Token{
+              id: %Oidcc.Token.Id{token: id_token, claims: claims},
+              access: %Oidcc.Token.Access{token: access_token},
+              refresh: refresh_token
+            }} <-
+             Oidcc.retrieve_token(
+               code,
+               Claper.OidcProviderConfig,
+               client_id(),
+               client_secret(),
+               opts(pkce_verifier, expected_state, nil)
+             ),
+           {:ok, oidc_user} <- validate_user(id_token, access_token, refresh_token, claims) do
         conn
-        |> put_status(:unauthorized)
-        |> put_view(ClaperWeb.ErrorView)
-        |> render("csrf_error.html", %{error: "Authentication failed: #{inspect(reason)}"})
+        |> configure_session(renew: true)
+        |> UserAuth.log_in_user(oidc_user.user)
+      else
+        {:error, reason} ->
+          conn
+          |> put_status(:unauthorized)
+          |> put_view(ClaperWeb.ErrorView)
+          |> render("csrf_error.html", %{error: "Authentication failed: #{inspect(reason)}"})
+      end
     end
   end
 
@@ -79,14 +100,23 @@ defmodule ClaperWeb.UserOidcAuth do
     Application.get_env(:claper, ClaperWeb.Endpoint)[:base_url]
   end
 
-  defp opts() do
+  defp opts(pkce_verifier, state, nonce) do
     url = base_url()
 
     %{
       redirect_uri: "#{url}/users/oidc/callback",
       scopes: scopes(),
-      preferred_auth_methods: [:client_secret_basic, :client_secret_post]
+      state: state,
+      nonce: nonce,
+      preferred_auth_methods: [:client_secret_basic, :client_secret_post],
+      pkce_verifier: pkce_verifier,
+      require_pkce: true,
+      response_mode: "query"
     }
+  end
+
+  defp generate_pkce_verifier do
+    :crypto.strong_rand_bytes(32) |> Base.url_encode64(padding: false)
   end
 
   defp format_refresh_token(%Oidcc.Token.Refresh{token: token}) do
